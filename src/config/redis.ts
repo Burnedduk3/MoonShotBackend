0b5ed6b8e9b15322c3649c16c6c029d7c2835fd0
Juan David Cabrera Duran <juandavid.juandis@gmial.com>
2020-07-19 16:33:55 -0500

    boosted redis functionality

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,15 +1,11 @@
 import { CONFIG_REDIS_HOST, CONFIG_REDIS_PASSWORD, CONFIG_REDIS_PORT } from '@config/variables';
-import { Tedis } from 'tedis';
+import redis, { RedisClient } from 'redis';
 
-const redisClient: Tedis | null = null;
+let client: RedisClient | null;
 
-export const Redis = (): Tedis => {
-  const client = redisClient
-    ? redisClient
-    : new Tedis({
-        host: CONFIG_REDIS_HOST,
-        port: CONFIG_REDIS_PORT,
-        password: CONFIG_REDIS_PASSWORD,
-      });
+export const Redis = (): RedisClient => {
+  if (!client) {
+    client = redis.createClient({ host: CONFIG_REDIS_HOST, port: CONFIG_REDIS_PORT, password: CONFIG_REDIS_PASSWORD });
+  }
   return client;
 };
